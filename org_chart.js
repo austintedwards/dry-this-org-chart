1eb7a4898302cfc953410df2fa3fc4bff9a1d3f6
Austin Edwards <austintedwards@gmail.com>
2017-03-21 14:03:42 -0600

    working now

--- org_chart.js
+++ org_chart.js
@@ -1,4 +1,4 @@
-function Employee(name, email, position, project,gender){
+function Employee(name, email, position, project, gender){
     this.name = name;
     this.email = email;
     this.position = position;
@@ -7,61 +7,48 @@ function Employee(name, email, position, project,gender){
     this.skills =[];
 }
 
-Employee.prototype.manager = function(manager){
+Employee.prototype.mrManager = function(manager){
   this.manager = manager;
 };
 
-let elon = new Employee("Elon Musk","elon.musk@musky.com","Boss")
-
-let hire = [new Employee("Alan Turing","alan.turing@musky.com","Computer Scientist","Cryptography","M"),
-new Employee("Grace Hopper","grace.hopper@musky.com","Computer Scientist","Compilers","F"),
-new Employee("Donald Knuth","donald.knuth@musky.com","Mathematician","Algorithm Analysis","M")]
-
-let [alan,grace,donald] = hire
-
-manager(alan,elon)
-manager(grace,elon)
-manager(donald,elon)
-
-function manager(hire,boss){
-  hire.manager = boss
-}
 // send to training
 
-function skills(hire,skill){
-  if(!hire.skills){
-    hire.skills =[]
-    hire.skills.push(skill)
-  }else{
-    hire.skills.push(skill)
+Employee.prototype.trade = function(skill){
+    this.skills.push(skill);
+};
+
+
+Employee.prototype.introduce = function(){
+    console.log(` ${this.name} is a ${this.position} working on ${this.project}.`);
+    console.log(` ${this.gender === 'F'? 'She':'He'} can be reached at ${this.email}.`);
+    console.log(`${this.gender === 'F'? 'Her':'His'} boss is ${this.manager.name} and skills include ${this.skills}. `);
   }
+
+  let elon = new Employee("Elon Musk","elon.musk@musky.com","Boss");
+
+  let hire = [new Employee("Alan Turing","alan.turing@musky.com","Computer Scientist","Cryptography","M"),
+  new Employee("Grace Hopper","grace.hopper@musky.com","Computer Scientist","Compilers","F"),
+  new Employee("Donald Knuth","donald.knuth@musky.com","Mathematician","Algorithm Analysis","M")];
+
+
+
+
+function hired(employee, manager, trade){
+  employee.mrManager(manager)
+  employee.trade(trade)
+  employee.introduce()
 }
 
-skills(alan,"Mathematics")
-skills(grace,"Cobol")
-skills(donald,"Computational Complexity")
+
+let [alan,grace,donald] = hire;
+hired(alan,elon,"Mathematics");
+hired(grace,elon,"Cobol");
+hired(donald,elon,"Computational Complexity");
+
+
 
 // whoops!  new person hired later on, do the same again for him
 
-let tim = new Employee ("Tim Berners-Lee","tim.berners_lee@musky.com","Computer Science","Networks")
-manager(tim,elon)
-skills(tim,"Protocols")
 
-console.log(listEmployes(hire))
-
-function listEmployes(employee){
-  let employeelist=""
-  for (var i = 0; i < employee.length; i++) {
-    employeelist+= ` ${employee[i].name} is a ${employee[i].position} working on ${employee[i].project}.`+
-    ` He can be reached at ${employee[i].email}. His boss is ${employee[i].manager.name} and skiils include ${employee[i].skills}. `
-  }
-  return employeelist
-}
-// console.log(`${alan.name} is a ${alan.position} working on ${alan.project}.`)
-// console.log(`He can be reached at ${alan.email}. His boss is ${alan.manager.name}`)
-// console.log(`${grace.name} is a ${grace.position} working on ${grace.project}.`)
-// console.log(`She can be reached at ${grace.email} and hes got skills in ${alan.skills}`)
-// console.log(`${donald.name} is a ${donald.position} working on ${donald.project}.`)
-// console.log(`He can be reached at ${donald.email}`)
-// console.log(`${tim.name} is a ${tim.position} working on ${tim.project}.`)
-// console.log(`He can be reached at ${tim.email}`)
+  let tim = new Employee ("Tim Berners-Lee","tim.berners_lee@musky.com","Computer Science","Networks");
+  hired(tim, elon, "Protocols");
